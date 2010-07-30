8d95e72feca14949fa1e688b04d3f2d718f7bc93
Alex Klementiev <aklement+github@gmail.com>
2010-07-30 17:53:02 -0400

    More changes.

--- figures/temporal/temporal.r
+++ figures/temporal/temporal.r
@@ -2,12 +2,14 @@
 te <- read.table(file="terrorist.en", sep = "\t", head=FALSE)
 ts <- read.table(file="terrorista.es", sep = "\t", head=FALSE)
 as <- read.table(file="ataques.es", sep = "\t", head=FALSE)
+rs <- read.table(file="riqueza.es", sep = "\t", head=FALSE)
 
-r <- range(te$V1, ts$V2, as$V2)
+r <- range(te$V1, ts$V2, as$V2, rs$V2)
 x <- c(r[1]:r[2])
 tey <- array(0, r[2] - r[1] + 1)
 tsy <- array(0, r[2] - r[1] + 1)
 asy <- array(0, r[2] - r[1] + 1)
+rsy <- array(0, r[2] - r[1] + 1)
   
 for (i in c(1:length(te$V1))) {
   tey[te[i, 1]] = te[i, 2]
@@ -21,14 +23,18 @@ for (i in c(1:length(as$V1))) {
   asy[as[i, 1]] = as[i, 2]
 }
 
+for (i in c(1:length(rs$V1))) {
+  rsy[rs[i, 1]] = rs[i, 2]
+}
+
 days = 0;
 for (i in c(1:length(tey))) {
-  if (tey[i] != 0 || tsy[i] != 0 || (asy[i] != 0)) {
+  if (tey[i] != 0 || tsy[i] != 0 || asy[i] != 0 || rsy[i] != 0) {
     days = days + 1;
   }
 }
 
-dd <- array(0, dim = c(days, 3))
+dd <- array(0, dim = c(days, 4))
 k <- 0
 
 for (i in c(1:length(tey))) {
@@ -41,7 +47,10 @@ for (i in c(1:length(tey))) {
   if (asy[i] != 0) {
     dd[k,3] <- -1 * asy[i]
   }
-  if (tey[i] != 0 || tsy[i] != 0 || asy[i] != 0) {
+  if (rsy[i] != 0) {
+    dd[k,4] <- -1 * rsy[i]
+  }
+  if (tey[i] != 0 || tsy[i] != 0 || asy[i] != 0 || rsy[i] != 0) {
     k = k+1;
   }
 }
@@ -49,12 +58,13 @@ for (i in c(1:length(tey))) {
 from <- 1
 to <- 210
 
-d <- array(0, dim = c(to-from+1, 3))
+d <- array(0, dim = c(to-from+1, 4))
 k <- 0
 for (i in c(from:to)) {
   d[k,1] <- dd[i,1]
   d[k,2] <- dd[i,2]
   d[k,3] <- dd[i,3]
+  d[k,4] <- dd[i,4]
   k = k+1;
 }
 
@@ -68,10 +78,11 @@ k = k/sum(k)
 d[,1] = filter(d[,1], sides=2, k) / sum(d[,1])
 d[,2] = - filter(d[,2], sides=2, k) / sum(d[,2])
 d[,3] = - filter(d[,3], sides=2, k) / sum(d[,3])
+d[,4] = - filter(d[,4], sides=2, k) / sum(d[,4])
 
 r <- range(te$V1, ts$V2)
 
-par(mar=c(0.25,2,2.25,0.5), mfrow=c(2,1))
+par(mar=c(0.25,2,0.25,0.5), mfrow=c(3,1), oma=c(2,0,0.25,0))
 
 plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
 lines(d[,2], type="h", col=colors[2], lty=1, lwd=1.5)
@@ -80,7 +91,7 @@ legend("bottomright", cex=0.8, c("terrorist (en)","terrorista (es)"), col=colors
 
 mtext("Occurrences", side=2,line=0.5,font=1)
 
-par(mar=c(2,2,0.25,0.5))
+par(mar=c(0.25,2,0.25,0.5))
 
 plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
 lines(d[,3], type="h", col=colors[2], lty=1, lwd=1.5)
@@ -88,6 +99,17 @@ lines(d[,3], type="h", col=colors[2], lty=1, lwd=1.5)
 legend("bottomright", cex=0.8, c("terrorist (en)","ataques (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
 
 mtext("Occurrences", side=2,line=0.5,font=1)
+#mtext("Time", side=1,line=0.5,font=1)
+
+par(mar=c(0.25,2,0.25,0.5))
+
+plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
+lines(d[,4], type="h", col=colors[2], lty=1, lwd=1.5)
+
+legend("bottomright", cex=0.8, c("terrorist (en)","riqueza (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
+
+mtext("Occurrences", side=2,line=0.5,font=1)
 mtext("Time", side=1,line=0.5,font=1)
+
 
 dev.off()
